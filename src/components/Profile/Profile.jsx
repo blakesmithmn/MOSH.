6d1b11ebd97e0288ba50a3f79f87fcc97d39171d
Blake Smith <blake.smith.mn@gmail.com>
2022-10-14 15:11:43 -0500

    profile page closer to a more usable format - still needs some margin adjustment as things are headed off the page ... and maybe fix text alignment?

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router'
 
-import { Paper, Card, CardContent, Typography, Button, ButtonGroup, CardActions, Box, Grid, CardMedia, FormGroup, TextField, Container, Avatar, IconButton } from '@mui/material';
+import { Paper, Card, CardContent, Typography, Button, ButtonGroup, ButtonBase, CardActions, Box, Grid, CardMedia, FormGroup, TextField, Container, Avatar, IconButton } from '@mui/material';
 import { useHistory } from 'react-router-dom';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { deepOrange, deepPurple, teal } from '@mui/material/colors';
@@ -33,31 +33,41 @@ function Profile() {
 
     return (
         <>
-            <Grid container spacing={4}>
-                <Grid item xs={12} sm={6} md={6} lg={4}>
-                    <Avatar sx={{ bgcolor: teal[500], width: 200, height: 200, fontSize: 100 }}>{user.first_name[0]}{user.last_name[0]}</Avatar>
+            <Grid container spacing={2}>
+                <Grid item>
+                    <ButtonBase sx={{ width: 128, height: 128 }}>
+                        <Avatar sx={{ bgcolor: teal[500], width: 200, height: 200, fontSize: 100 }}>{user.first_name[0]}{user.last_name[0]}</Avatar>
+                    </ButtonBase>
                 </Grid>
-                <Grid item xs={12} sm={6} md={6} lg={4}>
-                    <Card>
-                        <IconButton>
-                            <AddCircleIcon color='secondary' />
-                        </IconButton>
-                        <Typography variant='h5'>{user.first_name} {user.last_name}</Typography>
-                        <Button variant='contained' color='secondary' onClick={() => pushToEdit(user)}>EDIT PROFILE</Button>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6} lg={4}>
-                    <Card>
-
-                        <p>Current ZipCode: {user.zipcode}</p>
-                        <h2>About Me:  </h2>
-                        <p>{user.about_me}</p>
-                    </Card>
-                </Grid>
-                <Grid item xs={12}>
-                    <UpcomingEvents />
+                <Grid item xs={12} sm container>
+                    <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs>
+                            <Typography gutterBottom variant="h3" component="div">
+                                {user.first_name} {user.last_name}
+                            </Typography>
+                            <Typography variant="subtitle2" gutterBottom>
+                                {user.username}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {user.about_me}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Button variant='contained' color='secondary' onClick={() => pushToEdit(user)}>EDIT PROFILE</Button>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="subtitle1" component="div">
+                            {user.zipcode}
+                        </Typography>
+                    </Grid>
                 </Grid>
             </Grid>
+
+            <Grid item xs={12}>
+                <UpcomingEvents />
+            </Grid>
+
         </>
     )
 }
