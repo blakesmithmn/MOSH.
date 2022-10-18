9f5d8d2c6992bf57a5f512573a44849779f7cacb
Blake Smith <blake.smith.mn@gmail.com>
2022-10-18 14:19:35 -0500

    changed path a bit - going for a more simple edit page ... this one is responsive for mobile but we need to adjust the appearance of the avatar on mobile ...

--- src/components/Profile/MobileEdit.jsx
+++ src/components/Profile/MobileEdit.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router'
 import './Profile.css';
-import { Paper, Card, CardContent, Typography, Button, ButtonGroup, ButtonBase, CardActions, Box, Grid, CardMedia, FormGroup, TextField, Container, Avatar, IconButton, Stack } from '@mui/material';
+import { Paper, Card, CardContent, Typography, Button, ButtonGroup, CardActions, Box, Grid, CardMedia, FormGroup, TextField, Container, Avatar, IconButton, FormControl, InputLabel, Select, MenuItem, FormHelperText } from '@mui/material';
 import { useHistory } from 'react-router-dom';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
@@ -66,35 +66,55 @@ function MobileEdit() {
 
 
     return (
-        // <Grid container spacing={1} className='AppHeader' justifyContent="center" >
-        <Stack alignItems='center' className='MobileHeader'>
-            <Card xs={12} sx={{ display: { xs: 'inline', sm: 'inline', md: 'none' } }} >
-                <CardContent>
-                    <Avatar sx={{ bgcolor: getUserColor(profile), width: 150, height: 150, fontSize: 75 }}>{profile.id && <p>{profile.first_name[0]}</p>} {profile.id && <p>{profile.last_name[0]}</p>}</Avatar>
-                </CardContent>
-                <Typography variant="h3" component="div">
-                    {profile.first_name} {profile.last_name}
-                </Typography>
-                <CardContent>
-                    <Typography variant="subtitle2" >
-                        {profile.username}
-                    </Typography>
-                </CardContent>
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        {profile.about_me}
-                    </Typography>
-                </CardContent>
-                <CardContent>
-                    <Typography variant="subtitle1" className='zipcode'>
-                        {profile.zipcode}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button variant='contained' color='secondary' onClick={() => pushToEdit(user)}>EDIT PROFILE</Button>
-                </CardActions>
-            </Card>
-        </Stack>
+        <Grid container spacing={10} className='MobileHeader' justifyContent="center">
+            <Grid item>
+                <Avatar sx={{ bgcolor: getUserColor(user), width: 200, height: 200, fontSize: 100 }}>{user.first_name[0]}{user.last_name[0]}</Avatar>
+            </Grid>
+            <Grid item xs container spacing={4}>
+                <Grid item xs={6}>
+                    <TextField size='small' placeholder='First Name' value={profile.first_name || ''} onChange={(event) => dispatch({ type: 'EDIT_FIRST_NAME', payload: event.target.value })}></TextField>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField size='small' placeholder='Last Name' value={profile.last_name || ''} onChange={(event) => dispatch({ type: 'EDIT_LAST_NAME', payload: event.target.value })}></TextField>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField size='small' placeholder='About Me' value={profile.about_me || ''} onChange={(event) => dispatch({ type: 'EDIT_ABOUT_ME', payload: event.target.value })} fullWidth></TextField>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField size='small' placeholder='ZipCode' value={profile.zipcode || ''} onChange={(event) => dispatch({ type: 'EDIT_ZIPCODE', payload: event.target.value })}></TextField>
+                </Grid>
+                <Grid item xs={12} sm={7}>
+                    <FormControl fullWidth>
+                        <InputLabel>Color</InputLabel>
+                        <Select
+                            value={profile.color || ''}
+                            label="Pick a Color"
+                            onChange={(event) => dispatch({ type: 'EDIT_COLOR', payload: event.target.value })}
+                            required
+
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={'pink'}>Pink</MenuItem>
+                            <MenuItem value={'deepPurple'}>Purple</MenuItem>
+                            <MenuItem value={'indigo'}>Indigo</MenuItem>
+                            <MenuItem value={'teal'}>Teal</MenuItem>
+                            <MenuItem value={'green'}>Green</MenuItem>
+                            <MenuItem value={'orange'}>Orange</MenuItem>
+                            <MenuItem value={'lightBlue'}>Blue</MenuItem>
+                        </Select>
+                        <FormHelperText>Pick a Color for your Avatar</FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={6}>
+                    <Button variant='contained' onClick={handleApply}>APPLY</Button>
+                    <Button variant='contained' color='secondary' onClick={handleCancel}>CANCEL</Button>
+                </Grid>
+
+            </Grid>
+        </Grid>
+
     )
 }
 
